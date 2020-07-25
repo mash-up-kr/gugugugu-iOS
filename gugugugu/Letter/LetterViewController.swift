4c6c30c5f727054ea7e8c1a4cae03aa20f4a8ca4
JeongMinHo <wjdalsgh95@nate.com>
2020-07-26 03:02:59 +0900

    feat: 작성중 화면 클릭시 키보드 내리기

--- gugugugu/Letter/LetterViewController.swift
+++ gugugugu/Letter/LetterViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LetterViewController: UIViewController {
+class LetterViewController: UIViewController, UIGestureRecognizerDelegate {
     
     // MARK: - IBOutlet
     @IBOutlet weak var inputTextField: UITextField!
@@ -32,6 +32,8 @@ class LetterViewController: UIViewController {
         inputTextField.delegate = self
         letterTextView.delegate = self
         letterTextView.isHidden = true
+        addKeyboardObserver()
+        addTapGesture(to: self.view)
     }
     
     private func whenToTextFieldIsFilled() {
@@ -46,6 +48,7 @@ class LetterViewController: UIViewController {
             self.inputLabel.text = ""
             self.inputTextField.placeholder = "Title"
             self.letterCategoryLabel.text = "Title"
+            self.view.endEditing(true)
         })
         inputTextField.text = ""
     }
@@ -65,9 +68,35 @@ class LetterViewController: UIViewController {
             self.inputTextField.isHidden = true
             self.inputLabel.isHidden = true
             self.letterTextView.isHidden = false
+            self.view.endEditing(true)
         }
         inputTextField.text = ""
     }
+    
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+        self.view.frame.origin.y =  -150
+    }
+
+    @objc func keyboardWillHide(_ sender: Notification) {
+        self.view.frame.origin.y = 0
+    }
+    
+    private func addTapGesture(to view: UIView) {
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(dismissKeyboard))
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
@@ -75,6 +104,10 @@ class LetterViewController: UIViewController {
         
         setUp()
     }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.view.
+//    }
 }
 
 // MARK: - UITextFieldDelegate
