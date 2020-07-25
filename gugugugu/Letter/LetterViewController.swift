4b22b119bb69558c700288e1df319916cb25c073
JeongMinHo <wjdalsgh95@nate.com>
2020-07-25 17:48:33 +0900

    feat: LetterViewController 연결

---
+++ gugugugu/Letter/LetterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LetterViewController.swift
+//  gugugugu
+//
+//  Created by jeongminho on 2020/07/25.
+//  Copyright © 2020 JU HO YOON. All rights reserved.
+//
+
+import UIKit
+
+class LetterViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
