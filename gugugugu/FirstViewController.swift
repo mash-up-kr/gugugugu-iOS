e487170e8afcac27b2adf2b812f5f5c250eff5e9
YoonJuHo <xoxoinsight@gmail.com>
2020-07-25 15:42:58 +0900

    Initial Commit

---
+++ gugugugu/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  gugugugu
+//
+//  Created by JU HO YOON on 2020/07/25.
+//  Copyright © 2020 JU HO YOON. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ gugugugu/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  gugugugu
+//
+//  Created by JU HO YOON on 2020/07/25.
+//  Copyright © 2020 JU HO YOON. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
