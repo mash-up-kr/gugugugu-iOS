08bf0e3411ced397a854af075a70923a6f0d08db
JeongMinHo <wjdalsgh95@nate.com>
2020-07-25 21:45:19 +0900

    feat: add ToolBarButton

--- gugugugu/Letter/LetterViewController.swift
+++ gugugugu/Letter/LetterViewController.swift
@@ -10,8 +10,44 @@ import UIKit
 
 class LetterViewController: UIViewController {
     
+    // MARK: - IBOutlet
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    
+    // MARK: - IBAction
+    @IBAction func backButtonTouchUpInside(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Property
+    
+    // MARK: - Method
+    private func setUp() {
+        addToolBar()
+    }
+    
+    private func addToolBar() {
+        let toolBarKeyboard = UIToolbar()
+        toolBarKeyboard.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "확인", style: .done, target: self, action: #selector(doneButtonClicked))
+        
+        
+        let flexibleButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolBarKeyboard.items = [flexibleButton, doneButton]
+        inputTextField.inputAccessoryView = toolBarKeyboard
+    }
+    
+    @objc private func doneButtonClicked() {
+        print("Minho")
+    }
+    
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUp()
     }
 }
