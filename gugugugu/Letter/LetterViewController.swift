8ff1646dd93e8d2312d33f9121b1cf6367f7c22d
JeongMinHo <wjdalsgh95@nate.com>
2020-10-09 15:31:36 +0900

    feat: textview 추가하기

--- gugugugu/Letter/LetterViewController.swift
+++ gugugugu/Letter/LetterViewController.swift
@@ -12,6 +12,7 @@ class LetterToViewController: UIViewController, UIGestureRecognizerDelegate {
     
     // MARK: - IBOutlet
     @IBOutlet private weak var inputTextField: UITextField?
+    @IBOutlet private weak var inputTextView: UITextView?
     @IBOutlet private weak var toAndTitleView: UIView?
     @IBOutlet private weak var letterCategoryLabel: UILabel?
     @IBOutlet private weak var letterToLabel: UILabel?
@@ -32,6 +33,7 @@ class LetterToViewController: UIViewController, UIGestureRecognizerDelegate {
     // MARK: - Method
     private func setUp() {
         setView()
+        inputTextView?.delegate = self
         inputTextField?.delegate = self
         addKeyboardObserver()
         addTapGesture(to: self.view)
@@ -39,6 +41,9 @@ class LetterToViewController: UIViewController, UIGestureRecognizerDelegate {
     
     private func setView() {
         sequenceView?.backgroundColor = UIColor(red: 21, green: 21, blue: 22, alpha: 0)
+        
+        inputTextView?.isHidden = true
+        inputTextView?.textContainerInset = .zero
     }
     
     private func categoryToIsFilled() {
@@ -64,6 +69,7 @@ class LetterToViewController: UIViewController, UIGestureRecognizerDelegate {
         inputTextField?.placeholder = "내가 전달하고 싶은 이야기"
         currentPageLabel?.text = "3 / 4"
         seperationView?.isHidden = false
+        inputTextView?.isHidden = false
         
         view.endEditing(true)
         currentCategory = .letter
@@ -132,6 +138,26 @@ extension LetterToViewController: UITextFieldDelegate {
         default:
             return textFieldTextLength <= 100
         }
+    }
+}
+
+extension LetterToViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        guard let textViewText = textView.text else { return true }
+        let textViewTextLength = textViewText.count + text.count - range.length
+        
+        return textViewTextLength <= 200
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        guard let fixedWidth = inputTextView?.frame.size.width else { return }
+        let changedSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        textView.frame.size = CGSize(width: max(changedSize.width, fixedWidth), height: changedSize.height)
     }
 }
 
